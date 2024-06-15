c54356b59cf5e4d212272a3c666215d31d6a060f
Aaryan Kumar <119968129+Aaryan2705@users.noreply.github.com>
2024-06-15 20:34:24 +0530

    Time: 25 ms (11.26%), Space: 27.7 MB (31.27%) - LeetHub

---
+++ 0169-majority-element/0169-majority-element.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int majorityElement(vector<int>& nums) {
+        sort(nums.begin(),nums.end());
+        int n = nums.size();
+        return nums[n/2] ;
+        
+        
+    }
+};
+// public:
+//     int majorityElement(vector<int>& nums) {
+//         sort(nums.begin(), nums.end());
+//         int n = nums.size();
+//         return nums[n/2];
+//     }
+// };
