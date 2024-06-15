5cc864bbe373c966099b90cbadf6fe2751b4e348
Aaryan Kumar <119968129+Aaryan2705@users.noreply.github.com>
2024-06-15 18:50:14 +0530

    Time: 122 ms (52.48%), Space: 69.2 MB (24.24%) - LeetHub

--- 0945-minimum-increment-to-make-array-unique/0945-minimum-increment-to-make-array-unique.cpp
+++ 0945-minimum-increment-to-make-array-unique/0945-minimum-increment-to-make-array-unique.cpp
@@ -4,18 +4,16 @@ public:
         if (nums.size() <= 1) {
             return 0;
         }
-        
-        sort(nums.begin(), nums.end());
-        int moves = 0;
-        
-        for (int i = 1; i < nums.size(); ++i) {
-            if (nums[i] <= nums[i - 1]) {
-                int increment = nums[i - 1] - nums[i] + 1; // Calculate required increment
-                nums[i] += increment; // Increment nums[i] to make it unique
-                moves += increment; // Count the total moves
+        sort(nums.begin(),nums.end());
+        int moves = 0 ;
+        for (int i = 1 ; i < nums.size() ; i++){
+            if (nums[i] <= nums[i-1]) {
+                int increment = nums[i - 1] - nums[i] + 1;
+                nums[i] += increment; 
+                moves += increment;
+                
             }
         }
-        
-        return moves;
+        return moves ;
     }
-};
+};       
