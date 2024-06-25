f470bfab81942c4b2573de03a8038d21a8f2757d
Aaryan Kumar <119968129+Aaryan2705@users.noreply.github.com>
2024-06-25 16:17:54 +0530

    Time: 51 ms (94.03%), Space: 27.1 MB (79.42%) - LeetHub

--- 0015-3sum/0015-3sum.cpp
+++ 0015-3sum/0015-3sum.cpp
@@ -2,20 +2,15 @@ class Solution {
 public:
     vector<vector<int>> threeSum(vector<int>& nums) {
         vector<vector<int>> result;
+        sort(nums.begin(), nums.end());
         int n = nums.size();
         
-        // Sort the array
-        sort(nums.begin(), nums.end());
-        
-        // Iterate through each element as the first element of the triplet
         for (int i = 0; i < n - 2; ++i) {
-            // Avoid duplicates for nums[i]
             if (i > 0 && nums[i] == nums[i - 1])
                 continue;
             
-            int target = -nums[i];  // find two sum that equals to target
-            int left = i + 1;
-            int right = n - 1;
+            int target = -nums[i];
+            int left = i + 1, right = n - 1;
             
             while (left < right) {
                 int sum = nums[left] + nums[right];
@@ -24,18 +19,11 @@ public:
                 } else if (sum > target) {
                     --right;
                 } else {
-                    // Found a triplet
                     result.push_back({nums[i], nums[left], nums[right]});
-                    
-                    // Skip duplicate values of nums[left]
                     while (left < right && nums[left] == nums[left + 1])
                         ++left;
-                    
-                    // Skip duplicate values of nums[right]
                     while (left < right && nums[right] == nums[right - 1])
                         --right;
-                    
-                    // Move pointers to find next unique triplet
                     ++left;
                     --right;
                 }
