652a18d97a60272cf98d4807add17a0223d6dc70
Aaryan Kumar <119968129+Aaryan2705@users.noreply.github.com>
2024-06-28 12:07:05 +0530

    Time: 196 ms (99.66%), Space: 117.2 MB (84.83%) - LeetHub

---
+++ 2285-maximum-total-importance-of-roads/2285-maximum-total-importance-of-roads.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    long long maximumImportance(int n, vector<vector<int>>& roads) {
+        ios::sync_with_stdio(false);
+        cin.tie(nullptr);
+        cout.tie(nullptr);
+
+        vector<long long> deg(n, 0);
+        for (auto &a : roads) {
+            deg[a[0]]++;
+            deg[a[1]]++;
+        }
+        sort(deg.begin(), deg.end());
+        long long ans = 0;
+        for (long long i=0;i<n;i++) {
+            ans += ( (i+1)*deg[i] );
+        }
+        return ans;
+    }
+};
