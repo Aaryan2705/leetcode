04a1cea94b0502fb5e05c0a832628f016d3df545
Aaryan Kumar <119968129+Aaryan2705@users.noreply.github.com>
2024-07-12 21:47:40 +0530

    Time: 12 ms (13.37%), Space: 10.8 MB (5.26%) - LeetHub

--- 0345-reverse-vowels-of-a-string/0345-reverse-vowels-of-a-string.cpp
+++ 0345-reverse-vowels-of-a-string/0345-reverse-vowels-of-a-string.cpp
@@ -1,24 +1,19 @@
 class Solution {
 public:
     string reverseVowels(string s) {
-        unordered_set<char> vowels = {'a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U'};
-        stack<char> vowelStack;
-
-        // Traverse the string and push vowels onto the stack
-        for (int i = 0; i < s.length(); i++) {
-            if (vowels.find(s[i]) != vowels.end()) {
-                vowelStack.push(s[i]);
+        unordered_set<char> vowel = { 'a', 'e', 'i', 'o', 'u', 'A' , 'E' , 'I', 'O' , 'U'};
+        stack<char> collect;
+        for(int i = 0 ; i < s.length(); i++){
+            if(vowel.find(s[i]) != vowel.end()){
+                collect.push(s[i]);
             }
         }
-
-        // Traverse the string again and replace vowels with the ones from the stack
-        for (int i = 0; i < s.length(); i++) {
-            if (vowels.find(s[i]) != vowels.end()) {
-                s[i] = vowelStack.top();
-                vowelStack.pop();
+        for(int i = 0; i < s.length() ; i++){
+            if(vowel.find(s[i]) != vowel.end()){
+                s[i]=collect.top();
+                collect.pop();
             }
         }
-
         return s;
     }
 };
