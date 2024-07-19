9f8e32bc476729105c241f7b77a1df4df3d99c14
Aaryan Kumar <119968129+Aaryan2705@users.noreply.github.com>
2024-07-19 06:38:31 +0530

    Time: 0 ms (100.00%), Space: 8.7 MB (34.53%) - LeetHub

--- 1071-greatest-common-divisor-of-strings/1071-greatest-common-divisor-of-strings.cpp
+++ 1071-greatest-common-divisor-of-strings/1071-greatest-common-divisor-of-strings.cpp
@@ -1,11 +1,7 @@
 class Solution {
 public:
     string gcdOfStrings(string str1, string str2) {
-        //checking if the str1+str2 is equal to str2+str1 or not
-        //if not then gcd cannnot be find of such strings
         if(str1+str2 != str2+str1) return "";
-        // else we have to find the gcd .... gcd will always start from the starting
-        // the length of the gcd of the substring is equal to gcd of str1's length and str2's length
         return str1.substr(0,gcd(str1.length(),str2.length()));
     }
 };
