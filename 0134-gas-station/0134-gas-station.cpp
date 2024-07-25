bcd3440dae529ad8ab8a5e8a7b06d97a80a110eb
Aaryan Kumar <119968129+Aaryan2705@users.noreply.github.com>
2024-07-26 03:13:26 +0530

    Time: 98 ms (71.53%), Space: 130.8 MB (20.27%) - LeetHub

--- 0134-gas-station/0134-gas-station.cpp
+++ 0134-gas-station/0134-gas-station.cpp
@@ -5,9 +5,9 @@ public:
         int totalGas = 0, totalCost = 0, currentGas = 0, startIndex = 0;
 
         // Calculate the total gas and total cost
-        for (int i = 0; i < n; ++i) {
-            totalGas += gas[i];
-            totalCost += cost[i];
+        for (int i = 0; i < n; i++) {
+            totalGas = totalGas + gas[i];
+            totalCost = totalCost + cost[i];
         }
 
         // If the total gas is less than the total cost, it's impossible to complete the circuit
@@ -17,7 +17,7 @@ public:
 
         // Try to find the starting gas station
         for (int i = 0; i < n; ++i) {
-            currentGas += gas[i] - cost[i];
+            currentGas = currentGas + gas[i] - cost[i];
             // If currentGas is negative, reset it and set the next station as the starting point
             if (currentGas < 0) {
                 startIndex = i + 1;
