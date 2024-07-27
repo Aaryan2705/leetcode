416f75fd213e145c1c05502295819a99f9f2b48d
Aaryan Kumar <119968129+Aaryan2705@users.noreply.github.com>
2024-07-27 18:36:30 +0530

    Time: 12 ms (8.91%), Space: 37.8 MB (8.27%) - LeetHub

--- 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.cpp
+++ 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.cpp
@@ -3,14 +3,13 @@ public:
     string reverseWords(string s) {
         vector<string> words;
         string word;
-        
-        for(int i = 0 ; i < s.size();i++){
+        for(int i=0;i<s.size();i++){
             if(s[i]!=' '){
-                word = word + s[i];
-            } else if(!word.empty()){
+                word= word + s[i];               
+            }else if(!word.empty()){
                 words.push_back(word);
-                word.clear();
-            }            
+                word.clear();                
+            }
         }
         if(!word.empty()){
                 words.push_back(word);
@@ -18,12 +17,13 @@ public:
         reverse(words.begin(),words.end());
         
         string result;
-        for(int i =0 ; i < words.size();i++){
+        for(int i=0;i<words.size();i++){
             result = result + words[i];
             if(i<words.size()-1){
                 result = result + " ";
             }
         }
-        return result ;
+        return result;
+        
     }
 };
