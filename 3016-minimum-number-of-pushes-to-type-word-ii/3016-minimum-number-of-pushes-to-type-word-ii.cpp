4f4236f0f9d71b7ef1594d0ba2c5768a610fc6f2
Aaryan Kumar <119968129+Aaryan2705@users.noreply.github.com>
2024-08-06 07:12:31 +0530

    Time: 68 ms (79.47%), Space: 27.8 MB (14.43%) - LeetHub

--- 3016-minimum-number-of-pushes-to-type-word-ii/3016-minimum-number-of-pushes-to-type-word-ii.cpp
+++ 3016-minimum-number-of-pushes-to-type-word-ii/3016-minimum-number-of-pushes-to-type-word-ii.cpp
@@ -1,15 +1,24 @@
 class Solution {
 public:
     int minimumPushes(string word) {
-        vector<int> cnt(26);
-        for (char& c : word) {
-            ++cnt[c - 'a'];
+        vector<int> cnt(26, 0); // Array to store the frequency of each letter
+
+        // Count the frequency of each character in the word
+        for (int i = 0; i < word.size(); i++) {
+            cnt[word[i] - 'a']++;
         }
+
+        // Sort the frequency array in descending order
         sort(cnt.rbegin(), cnt.rend());
+
         int ans = 0;
-        for (int i = 0; i < 26; ++i) {
+
+        // Calculate the minimum pushes required
+        for (int i = 0; i < 26; i++) {
+            if (cnt[i] == 0) break; // Stop if there are no more letters
             ans += (i / 8 + 1) * cnt[i];
         }
+
         return ans;
     }
 };
