c175aaeda5723a4fee00bf6075eb0207787ad94d
Aaryan Kumar <119968129+Aaryan2705@users.noreply.github.com>
2024-10-02 21:12:24 +0530

    Time: 11 ms (48.61%), Space: 7.4 MB (34.36%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.cpp
+++ 0509-fibonacci-number/0509-fibonacci-number.cpp
@@ -1,7 +1,9 @@
 class Solution {
 public:
     int fib(int n) {
-        if (n <= 1) return n;  // Base cases: fib(0) = 0, fib(1) = 1
-        return fib(n - 1) + fib(n - 2);  // Recursive call for Fibonacci
+        if (n==0) return 0;
+        if(n==1) return 1;
+        return fib(n - 1) + fib(n - 2);
+        
     }
 };
